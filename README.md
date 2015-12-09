7017ad99f827e3901c29621ddf55e88bd355dd8b
codethejason <tigergoldfish17@gmail.com>
2015-12-09 17:18:41 -0500

    Basic application and structure completed

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # metadata-viewer
 
-Application takes in multiple files and displays the name, size, type and last modified date of them. It will also show the file on the screen if it is an image.  
+Application takes in multiple files and displays the name, size, type and last modified date of them. 
 
 ##To Use:
 1. Clone Repository  
 2. Navigate to index.html  
 3. Upload a File
 4. View Metadata
+
+##To Add:
+-CSS Styling
--- js/script.js
+++ js/script.js
@@ -3,14 +3,28 @@ document.addEventListener('DOMContentLoaded', function(){
     //set variables
     var input = document.getElementById('input');
     var basicInfo = document.getElementById("basicInfo");
+    var filesDiv = document.getElementById("files");
 
     //on change in input element execute function
     input.addEventListener("change", printMetaData, false);
 
     function printMetaData() {
+        var template = document.getElementById("fileTemplate");
         //set files json equal to the files uploaded
         var files = this.files;        
-        basicInfo.innerHTML = files.length; //display number of files
+        //display number of files
+        basicInfo.innerHTML = "You have just uploaded " + files.length + "file(s)."; 
+        for(var counter = 0; counter < files.length; counter++) {
+            //clone the template inside content into a new div element
+            var tmpl = template.content.cloneNode(true);
+            //add the information to the appropriate divs
+            tmpl.querySelector('.name').innerHTML = files[counter].name;
+            tmpl.querySelector('.size').innerHTML = files[counter].size;
+            tmpl.querySelector('.type').innerHTML = files[counter].type;
+            tmpl.querySelector('.date').innerHTML = files[counter].lastModifiedDate;
+            //append the file div
+            filesDiv.appendChild(tmpl);
+        }
     }
     
 });
